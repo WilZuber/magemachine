6fb5a5f091aac1249d698094d0b048c3d90e0f34
boredomcrusher <Matrix1000atr9@gmail.com>
2023-10-26 19:02:38 -0700

    Level exit script now loads the next level.

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using System;
 
-public class LevelExit: MonoBehaviour 
+public class LevelExit: MonoBehaviour
 {
+    public static string[] levelList = {"Main Menu", "Tutorial", "Level1", "Level2"};
+
+    // This assumes the script is being used on the Tutorial level first.
+    public static int currentLevel = 1;
     public gameObject endLevelText;
 
     void Start() {
@@ -18,6 +23,15 @@ public class LevelExit: MonoBehaviour
             endLevelText.SetActive(true);
             
             // Add code here for a prompt to start the next level.
+            if (Input.GetKeyCode(KeyCode.R)) {
+                if (currentLevel < 3) {
+                    currentLevel += 1;
+                    SceneManager.LoadScene(levelList[currentLevel]);
+                } else {
+                    print("ERROR: There are only four levels currently.");
+                }
+                
+            }
         }
     }
 }
