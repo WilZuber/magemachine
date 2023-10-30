777503aec9b2ecdebf081e5fc0d26a881e2e5132
Igor R <irudenk@gmail.com>
2023-10-29 21:01:19 -0700

    animation controller setup

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
+    private Animator anim;
+    private Rigidbody rb; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        anim.SetFloat("HVelocity", rb.velocity.x);
+        anim.SetFloat("VVelocity", rb.velocity.z);
     }
 }
