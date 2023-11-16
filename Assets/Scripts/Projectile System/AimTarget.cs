0dde97d8cd6a8600cecd30663a8d506fb369a3b6
Astraea <rollinj4@wwu.edu>
2023-11-15 22:25:06 -0800

    Improve aiming

---
+++ Assets/Scripts/Projectile System/AimTarget.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimTarget : MonoBehaviour
+{
+    public Vector3 target; //use as a point or direction depending on whether there is a valid target
+    public bool validTarget;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new(transform.position + 2 * transform.forward, transform.forward);
+        validTarget = Physics.Raycast(ray, out RaycastHit hit);
+        target = validTarget ? hit.point : transform.forward;
+    }
+}
