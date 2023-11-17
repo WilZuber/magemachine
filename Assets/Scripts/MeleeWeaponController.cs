695a3e3c250d86bf47175d0708af55bba4cd83e7
Igor R <irudenk@gmail.com>
2023-11-16 22:44:07 -0800

    melee stuff kinda works

--- Assets/Scripts/MeleeWeaponController.cs
+++ Assets/Scripts/MeleeWeaponController.cs
@@ -7,35 +7,40 @@ public class MeleeWeaponController : MonoBehaviour
     private Animator anim;
     public GameObject weapon;
     private bool canAttack;
-
+    private KeyCode meleeKey = KeyCode.F;
     // Start is called before the first frame update
     void Start()
     {
-        //set anim
+        anim = GetComponent<Animator>();
         canAttack = true;
     }
 
+    void Update()
+    {
+        if (canAttack && Input.GetKey(meleeKey))
+        {
+            Attack();
+        }
+    }
     //Start attack process
     public void Attack()
     {
-        if (canAttack)
-        {
-            canAttack = false;
-            //start animation
-            Invoke(nameof(Activate), 0.25f);
-        }
+        canAttack = false;
+        Invoke(nameof(Activate), 0.25f);
     }
 
     //Activate weapon after reaching start of swing
     private void Activate()
     {
         weapon.SetActive(true);
+        anim.SetBool("MeleeAtk", true);
         Invoke(nameof(Deactivate), 0.5f);
     }
 
     //Deactivate weapon at end of swing
     private void Deactivate()
     {
+        anim.SetBool("MeleeAtk", false);
         weapon.SetActive(false);
         canAttack = true;
     }
