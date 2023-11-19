b1a2e624a8edcc99445c9944e3dc33dd53c23d19
Wilson <WinterZuber11@gmail.com>
2023-11-18 22:20:58 -0800

    Make camera work like cinmachine

---
+++ Assets/Scripts/DIYCinmachine.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DIYCinmachine : MonoBehaviour
+{
+    private Transform pivot;
+    private Ray ray;
+    private RaycastHit hit;
+    private float distance = 1.5f;
+    private Vector3 direction;
+
+    void Start()
+    {
+        pivot = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = -transform.forward;
+        ray = new Ray(pivot.position, direction);
+        Debug.DrawRay(ray.origin, ray.direction*distance, Color.red);
+
+        if (Physics.Raycast(ray, out hit) && (hit.distance < distance))
+        {
+            transform.position = hit.point;
+        }
+        else
+        {
+            transform.position = distance * direction + pivot.position;
+        }
+    }
+}
