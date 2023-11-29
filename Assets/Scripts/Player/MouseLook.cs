f8c3336a1613658068f27dc67a545093a9de501f
Astraea <rollinj4@wwu.edu>
2023-11-29 14:25:13 -0800

    Add vertical camera clamping

--- Assets/Scripts/Player/MouseLook.cs
+++ Assets/Scripts/Player/MouseLook.cs
@@ -6,6 +6,9 @@ public class MouseLook : MonoBehaviour
 {
     public float lookSpeedX = 5f;
     public float lookSpeedY = 3f;
+    public float minV = -90f;
+    public float maxV = 90f;
+    public Vector3 angles;
     public GameObject player;
 
     // Start is called before the first frame update
@@ -13,6 +16,7 @@ public class MouseLook : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        //minV += 360;
     }
 
     // Update is called once per frame
@@ -21,6 +25,17 @@ public class MouseLook : MonoBehaviour
         float lookX = lookSpeedX * Input.GetAxis("Mouse X");
         float lookY = lookSpeedY * Input.GetAxis("Mouse Y");
         player.transform.Rotate(0, lookX, 0);
-        transform.Rotate(-lookY, 0, 0);
+
+        angles = transform.rotation.eulerAngles;
+        //flip if necessary
+        float currentV = (angles.z < 170) ? angles.x : 180 - angles.x;
+        //Map from  [0, 360) to (-180, 180]
+        if (currentV > 180)
+        {
+            currentV -= 360;
+        }
+        float nextV = currentV - lookY;
+        nextV = Mathf.Clamp(nextV, minV, maxV);
+        transform.Rotate(nextV - currentV, 0, 0);
     }
 }
