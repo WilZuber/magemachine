8c543c72abdf4a863f6bf07f8ab3a07b78c50c27
carya2 <carya2@wwu.edu>
2023-12-04 18:56:29 -0800

    teleporter gun programmed.

--- Assets/Scripts/Teleporting/TeleportGun.cs
+++ Assets/Scripts/Teleporting/TeleportGun.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeleportGun : MonoBehaviour
+public class TeleportGun : GunInstance
 {
     public static GameObject player;
     public static GameObject teleportPoint;
---
+++ Assets/Scripts/Teleporting/TeleportType.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportType : GunType
+{
+    void Awake()
+    {
+        SetProjectileType(TestProjectile2());
+        SetModel(2);
+        reloadDuration = 0.5f;
+    }
+
+    private ProjectileType TestProjectile2()
+    {
+        LinearBlink blink = LinearBlink.New();
+        Bullet bullet = RedBullet.New();
+        Bullet child = RedBullet.New();
+        child.damage = 5;
+        Explosion explosion = Explosion.New();
+
+        child.next.Add(explosion);
+        bullet.next.Add(child);
+        blink.next.Add(bullet);
+
+        return blink;
+    }
+}
