db3e93b04cf1f5d724ba608668f4e2493c506495
carya2 <carya2@wwu.edu>
2023-12-06 16:57:05 -0800

    fixed the stamina bug and successfully changed run and movespeed.

--- Assets/Scripts/Menus/HUDUpdateStaminaSprite.cs
+++ Assets/Scripts/Menus/HUDUpdateStaminaSprite.cs
@@ -17,14 +17,24 @@ public class HUDUpdateStaminaSprite : MonoBehaviour
     void Update()
     {
         UpdateStaminaMeterSprite();
-        print(playerStaminaManager.GetStamina());
+        //print(playerStaminaManager.GetStamina());
+        //print(playerStaminaManager.hasStamina);
     }
 
     public void UpdateStaminaMeterSprite()
     {
         float maxStamina = playerStaminaManager.GetMaxStamina();
         float currentStamina = playerStaminaManager.GetStamina();
-        float currentStaminaUsedRatio = 1-currentStamina/maxStamina;
+        float currentStaminaUsedRatio;
+
+        // stamina is intentionally set to a negative value if the player runs out of stamina,
+        // to make the player wait a moment before stamina refill, so this makes the HUD element more accurate,
+        // making the stamina meter empty if the player doesn't have any stamina yet
+        if (currentStamina >= 0) {
+            currentStaminaUsedRatio = 1-currentStamina/maxStamina;
+        } else {
+            currentStaminaUsedRatio = 1;
+        }
 
         int currentSpriteIndex = ((int) (currentStaminaUsedRatio*18)); // should be a number between 0 and 17.
 
