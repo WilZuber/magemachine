da786733a9aaa2b6daabf13377f0932fc5fb3b78
Wilson <WinterZuber11@gmail.com>
2023-12-10 13:29:47 -0800

    Add AI agent to the new AI scripts

--- Assets/Scripts/AI/NewAISystem/MeleeAIContext.cs
+++ Assets/Scripts/AI/NewAISystem/MeleeAIContext.cs
@@ -4,6 +4,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 // Contexts in the State Pattern represent the system. Think of a State Machine Diagram. The entire diagram would be the context.
 public class MeleeAIContext : MonoBehaviour
@@ -12,7 +13,27 @@ public class MeleeAIContext : MonoBehaviour
     private IMeleeAIState lookState; // in the future we may look into making the states singletons
     private IMeleeAIState attackState;
     private IMeleeAIState chaseState;
+    private IMeleeAIState patrolState;
 
+    private NavMeshAgent agent;
+
+    // On start we want to do a few things:
+    // Find our Melee AI agent and assign it to our field
+    // Initialize lookState, attackState, patrolState and chaseState to be 
+    //          references to the corresponding Concrete States
+    // Initialize currentState to patrol.
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        lookState = new MeleeAILookState(this);
+        attackState = new MeleeAIAttackState(this);
+        chaseState = new MeleeAIChaseState(this);
+        patrolState = new MeleeAIPatrolState(this);
+
+        SetState(patrolState); // we want to start off patrolling
+    }
+
+    // STATE PATTERN METHODS
     public void SetState(IMeleeAIState state)
     {
         currentState = state;
@@ -37,4 +58,6 @@ public class MeleeAIContext : MonoBehaviour
     {
         currentState.PlayerLeaveMeleeRange();
     }
+
+    // AI BEHAVIOR METHODS
 }
