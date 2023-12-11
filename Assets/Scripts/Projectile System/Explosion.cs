d0a10361cba0cc27e59417a2dddd146869986530
carya2 <carya2@wwu.edu>
2023-12-10 22:49:01 -0800

    deleted a comment

--- Assets/Scripts/Projectile System/Explosion.cs
+++ Assets/Scripts/Projectile System/Explosion.cs
@@ -24,7 +24,6 @@ public class Explosion : ProjectileType
         
         if (other.TryGetComponent(out HealthManager hp)) // if the object has a HealthManager
         {
-            // explosion sound effect only happens when something is hit by it
             hp.TakeDamage(damage);
         }
     }
